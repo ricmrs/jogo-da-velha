e7fb208fbddadf24de4c06b4166a819ae00cd959
Ricardo Macedo <ricardomrs@gmail.com>
2021-10-27 17:27:07 -0300

    Create game modes

--- script.js
+++ script.js
@@ -2,25 +2,44 @@ let pos = window.document.querySelectorAll('.pos');
 let round = 0;
 let occupied = []
 let res = window.document.querySelector('.result');
+let mode = window.document.querySelector('#bot');
+let modepvp = window.document.querySelector('#pvp');
 
 for(var i = 0; i < 9; i++){
     occupied.push(0);
 }
 
 for(let i = 0; i < pos.length; i++) {
-    pos[i].addEventListener('click', a => {
-        if(occupied[i] == 0){
-            pos[i].insertAdjacentText('beforeend', 'X');
-            occupied[i] = 1; 
-            round++; 
-            checkEnd();
-            setTimeout(() => {bot()}, 100);        
-        }         
-    });
+        pos[i].addEventListener('click', a => {
+            if(mode.checked){
+                modepvp.setAttribute("disabled", "");
+                if(occupied[i] == 0){
+                    pos[i].insertAdjacentText('beforeend', 'X');
+                    occupied[i] = 1; 
+                    round++; 
+                    checkEnd();
+                    setTimeout(() => {bot()}, 100);  //VER EMPATE !      
+                }
+            } else {
+                mode.setAttribute("disabled", "");
+                if(occupied[i] == 0){
+                    if(round % 2 == 0){
+                    pos[i].insertAdjacentText('beforeend', 'X');
+                    occupied[i] = 1;
+                    } else {
+                    pos[i].insertAdjacentText('beforeend', 'O');
+                    occupied[i] = 2;
+                    }
+                    round++;
+                    checkEnd();       
+                }
+            }           
+        });
 }
 
+
 function bot() {
-    if(round != 5){
+    if(occupied[i] != 3){
         var x = parseInt(Math.random() * 9);
         if(occupied[x] == 0){            
             pos[x].insertAdjacentText('beforeend', 'O');
@@ -33,22 +52,20 @@ function bot() {
 }
 
 function checkEnd() {
-    if(round == 5){
-        res.textContent = 'Empate!';
-        btnReset.style.visibility = "visible";
-        res.style.visibility = "visible";
-    }
-
     for(i = 0; i <= 6; i += 3){ //rows
         if(occupied[i] == 1 && occupied[i+1] == 1 && occupied[i+2] == 1){
-            res.textContent = 'Você ganhou!';
-            pos[i].style.color = 'red';
-            pos[i+1].style.color = 'red';
-            pos[i+2].style.color = 'red';
+            if(mode.checked){ res.textContent = 'Você ganhou!';
+            } else { res.textContent = 'Jogador 1 ganhou!' }
+            res.style.color = 'green';
+            pos[i].style.color = 'green';
+            pos[i+1].style.color = 'green';
+            pos[i+2].style.color = 'green';
             endsGame();
         }
         if(occupied[i] == 2 && occupied[i+1] == 2 && occupied[i+2] == 2){
-            res.textContent = 'Você perdeu!';
+            if(mode.checked){ res.textContent = 'Você perdeu!';
+            } else { res.textContent = 'Jogador 2 ganhou!' }
+            res.style.color = 'red';
             pos[i].style.color = 'red';
             pos[i+1].style.color = 'red';
             pos[i+2].style.color = 'red';
@@ -57,14 +74,18 @@ function checkEnd() {
     }
     for(i = 0; i <= 2; i++){ //columns
         if(occupied[i] == 1 && occupied[i+3] == 1 && occupied[i+6] == 1){
-            res.textContent = 'Você ganhou!';
-            pos[i].style.color = 'red';
-            pos[i+3].style.color = 'red';
-            pos[i+6].style.color = 'red';
+            if(mode.checked){ res.textContent = 'Você ganhou!';
+            } else { res.textContent = 'Jogador 1 ganhou!' }
+            res.style.color = 'green';
+            pos[i].style.color = 'green';
+            pos[i+3].style.color = 'green';
+            pos[i+6].style.color = 'green';
             endsGame();
         }
         if(occupied[i] == 2 && occupied[i+3] == 2 && occupied[i+6] == 2){
-            res.textContent = 'Você perdeu!';
+            if(mode.checked){ res.textContent = 'Você perdeu!';
+            } else { res.textContent = 'Jogador 2 ganhou!' }
+            res.style.color = 'red';
             pos[i].style.color = 'red';
             pos[i+3].style.color = 'red';
             pos[i+6].style.color = 'red';
@@ -73,20 +94,35 @@ function checkEnd() {
     }
     for(i=2; i <= 4; i += 2){ //cross
         if(occupied[4-i] == 1 && occupied[4] == 1 && occupied[4+i] == 1){
-            res.textContent = 'Você ganhou!';
-            pos[4-i].style.color = 'red';
-            pos[4].style.color = 'red';
-            pos[4+i].style.color = 'red';
+            if(mode.checked){ res.textContent = 'Você ganhou!';
+            } else { res.textContent = 'Jogador 1 ganhou!' }
+            res.style.color = 'green';
+            pos[4-i].style.color = 'green';
+            pos[4].style.color = 'green';
+            pos[4+i].style.color = 'green';
             endsGame();
         }
         if(occupied[4-i] == 2 && occupied[4] == 2 && occupied[4+i] == 2){
-            res.textContent = 'Você perdeu!';
+            if(mode.checked){ res.textContent = 'Você perdeu!';
+            } else { res.textContent = 'Jogador 2 ganhou!' }
+            res.style.color = 'red';
             pos[4-i].style.color = 'red';
             pos[4].style.color = 'red';
             pos[4+i].style.color = 'red';
             endsGame();
         }
     }
+    if(mode.checked){
+        if(round == 5 && res.style.visibility == "hidden"){
+            res.textContent = 'Empate!';
+            endsGame();
+        }
+    } else {
+        if (round == 9 && res.style.visibility == "hidden"){
+            res.textContent = 'Empate!';
+            endsGame();
+        }
+    }    
 }
 
 function endsGame(){
@@ -95,6 +131,8 @@ function endsGame(){
     }
     btnReset.style.visibility = "visible";
     res.style.visibility = "visible";
+    mode.removeAttribute("disabled", "");
+    modepvp.removeAttribute("disabled", "");
 }
 
 let btnReset = window.document.querySelector('.reset');
@@ -104,8 +142,9 @@ btnReset.addEventListener('click', function(){
         pos[i].textContent = "";
         pos[i].style.color = '#000';
         occupied[i] = 0;
-        round = 0;
     }
+    round = 0;
     btnReset.style.visibility = "hidden";
     res.style.visibility = "hidden";
+    res.style.color = "#000";
 })
