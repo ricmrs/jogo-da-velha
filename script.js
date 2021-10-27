3b749ce9c26bce7c55f4b63349e91336fc32a25e
Ricardo Macedo <ricardomrs@gmail.com>
2021-10-26 21:38:26 -0300

    Add reset button and red color when game ends

--- script.js
+++ script.js
@@ -26,30 +26,48 @@ function checkEnd() {
     for(i = 0; i <= 6; i += 3){ //rows
         if(occupied[i] == 1 && occupied[i+1] == 1 && occupied[i+2] == 1){
             console.log("X ganha");
+            pos[i].style.color = 'red';
+            pos[i+1].style.color = 'red';
+            pos[i+2].style.color = 'red';
             ocp();
         }
         if(occupied[i] == 2 && occupied[i+1] == 2 && occupied[i+2] == 2){
             console.log("O ganha");
+            pos[i].style.color = 'red';
+            pos[i+1].style.color = 'red';
+            pos[i+2].style.color = 'red';
             ocp();
         }
     }
     for(i = 0; i <= 2; i++){ //columns
         if(occupied[i] == 1 && occupied[i+3] == 1 && occupied[i+6] == 1){
             console.log("X ganha");
+            pos[i].style.color = 'red';
+            pos[i+3].style.color = 'red';
+            pos[i+6].style.color = 'red';
             ocp();
         }
         if(occupied[i] == 2 && occupied[i+3] == 2 && occupied[i+6] == 2){
             console.log("O ganha");
+            pos[i].style.color = 'red';
+            pos[i+3].style.color = 'red';
+            pos[i+6].style.color = 'red';
             ocp();
         }
     }
     for(i=2; i <= 4; i += 2){ //cross
         if(occupied[4-i] == 1 && occupied[4] == 1 && occupied[4+i] == 1){
             console.log("X ganha");
+            pos[4-i].style.color = 'red';
+            pos[4].style.color = 'red';
+            pos[4+i].style.color = 'red';
             ocp();
         }
         if(occupied[4-i] == 2 && occupied[4] == 2 && occupied[4+i] == 2){
             console.log("O ganha");
+            pos[4-i].style.color = 'red';
+            pos[4].style.color = 'red';
+            pos[4+i].style.color = 'red';
             ocp();
         }
     }
@@ -60,3 +78,14 @@ function ocp(){
         occupied[i] = 3;
     }
 }
+
+let btnReset = window.document.querySelector('.reset')
+
+btnReset.addEventListener('click', function(){
+    for(var i = 0; i < 9; i++){
+        pos[i].textContent = "";
+        pos[i].style.color = '#000';
+        occupied[i] = 0;
+        round = 0;
+    }
+})
